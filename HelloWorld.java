785e34fa5cbff97486ed2874a2d081bba79b4723
Ciprion Devine <Ciprion.Devine@students.ittralee.ie>
2019-11-27 09:45:05 +0000

    HellowWorld

---
+++ HelloWorld.java
@@ -0,0 +1,18 @@
+/**
+ * @(#)HelloWorld.java
+ *
+ *
+ * @author 
+ * @version 1.00 2019/9/11
+ */
+
+//this is programe text called Hello World.
+
+public class HelloWorld {
+	public static void main (String args[]){
+ //String is called Hello World as it is a tearm.
+		System.out.println("Hello World!"); 
+ 		
+ }
+    
+}
